d41b66c7bf0253fa3136eed333d794ae14949926
FooChao <e1121427@u.nus.edu>
2024-09-27 12:48:13 +0800

    Fix problem 3 error 0-16

--- src/problem3/refactoredCode.tsx
+++ src/problem3/refactoredCode.tsx
@@ -1,35 +1,42 @@
+import React, { useState, useEffect, useMemo } from 'react'; // Error 0 fix
+import { BoxProps } from './unknown';
+import useWalletBalances from './unknown'; 
+import WalletRow from './unknown'; 
+import classes from './unknown'; //error 16 fix
+
 interface WalletBalance {
     currency: string;
     amount: number;
+    blockchain: string; //Error 10 fix
   }
-  interface FormattedWalletBalance {
+  interface FormattedWalletBalance { //Error 2 (not fixed due to lack of information)
     currency: string;
     amount: number;
-    formatted: string;
+    formattedAmount: string; //Error 1 fix
+    blockchain: string; //Error 10 carry-forward assuming error 2 is valid
   }
   
   class Datasource {
     // TODO: Implement datasource class
   }
   
-  interface Props extends BoxProps {
-  
-  }
-  const WalletPage: React.FC<Props> = (props: Props) => {
+  //Error 3 fix 
+
+  const WalletPage: React.FC<BoxProps> = (props: BoxProps) => {
     const { children, ...rest } = props;
     const balances = useWalletBalances();
-      const [prices, setPrices] = useState({});
+      const [prices, setPrices] = useState([]); //Error 4 fix
   
     useEffect(() => {
       const datasource = new Datasource("https://interview.switcheo.com/prices.json");
-      datasource.getPrices().then(prices => {
-        setPrices(prices);
+      datasource.getPrices().then(fetchedPrices => { // Error 5 partial fix
+        setPrices(fetchedPrices);
       }).catch(error => {
-        console.err(error);
+        console.error(error); // Error 6 fix
       });
     }, []);
   
-      const getPriority = (blockchain: any): number => {
+      const getPriority = (blockchain: string): number => { //Error 7 fix
         switch (blockchain) {
           case 'Osmosis':
             return 100
@@ -49,7 +56,7 @@ interface WalletBalance {
     const sortedBalances = useMemo(() => {
       return balances.filter((balance: WalletBalance) => {
             const balancePriority = getPriority(balance.blockchain);
-            if (lhsPriority > -99) {
+            if (balancePriority > 0) { //error 8-9 fix
                if (balance.amount <= 0) {
                  return true;
                }
@@ -63,13 +70,14 @@ interface WalletBalance {
             } else if (rightPriority > leftPriority) {
               return 1;
             }
+            return 0; // error 11 fix
       });
-    }, [balances, prices]);
+    }, [balances]); //error 12 fix
   
-    const formattedBalances = sortedBalances.map((balance: WalletBalance) => {
+    const formattedBalances : FormattedWalletBalance = sortedBalances.map((balance: WalletBalance) => { //error 13 not fixed due to lack of information and error 14 fix
       return {
         ...balance,
-        formatted: balance.amount.toFixed()
+        formattedAmount: balance.amount.toFixed() // error 15 not fixed due to lack of informtion
       }
     })
   
@@ -81,7 +89,7 @@ interface WalletBalance {
           key={index}
           amount={balance.amount}
           usdValue={usdValue}
-          formattedAmount={balance.formatted}
+          formattedAmount={balance.formattedAmount}
         />
       )
     })
